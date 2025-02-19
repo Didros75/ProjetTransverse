ef059c4641ef5d92c1d6b11d0789b81133e84099
julie <julie.sieux@efrei.net>
2025-02-19 17:30:41 +0100

    collisions axe y

--- Map.py
+++ Map.py
@@ -1,9 +1,9 @@
 import csv
 import pygame
 
-white= pygame.image.load('carrés/jaune.png')
-black=pygame.image.load('carrés/vert.png')
-color = pygame.image.load('carrés/noir.jpg')
+ciel = pygame.image.load('carrés/ciel.jpg')
+herbe =pygame.image.load('carrés/herbes.jpg')
+sol = pygame.image.load('carrés/sol.jpg')
 
 class Tile() :
     def __init__(self, image, x, y) :
@@ -11,13 +11,15 @@ class Tile() :
         self.pos_x = y
         self.pos_y = x
 
+        self.rectangle = pygame.Rect(y, x, 30, 30)
+
     def draw(self, surface) :
 
         surface.blit(self.image, (self.pos_x, self.pos_y))
 
 class Create_map() :
     def __init__(self, filename, screen) :
-        self.size_tile = 20
+        self.size_tile = 30
         self.tiles = self.load_tiles(filename)
         self.surface = screen
         self.load_map()
@@ -42,16 +44,14 @@ class Create_map() :
         for x in range(len(map)) :
             for y in range(len(map[x])) :
                 if map[x][y] == "-1" :
-                    tiles.append(Tile(black, x * self.size_tile, y * self.size_tile))
+                    tiles.append(Tile(ciel, x * self.size_tile, y * self.size_tile))
                 elif map[x][y] == "1" :
-                    tiles.append(Tile(white, x * self.size_tile, y * self.size_tile))
+                    tiles.append(Tile(herbe, x * self.size_tile, y * self.size_tile))
                 elif map[x][y] == "2" :
-                    tiles.append(Tile(color, x * self.size_tile, y * self.size_tile))
+                    tiles.append(Tile(sol, x * self.size_tile, y * self.size_tile))
         return tiles
 
     def load_map(self) :
         for i in range(len(self.tiles)) :
             self.tiles[i].draw(self.surface)
-
-    def draw_map(self, surface) :
-        surface.blit(self.surface, (0, 0))
+        return self.tiles
