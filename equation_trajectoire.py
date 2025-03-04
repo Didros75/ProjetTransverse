0054437d37b0a33bc61dfe933da0d4dd9ea8165c
Arthur <arthur.morineaux@efrei.net>
2025-03-04 12:29:14 +0100

    juste deux trois modif et ajout de l'inclinaision de la fleche

--- equation_trajectoire.py
+++ equation_trajectoire.py
@@ -1,10 +1,13 @@
 from math import*
 
+import equation_trajectoire
+
+
 #équation de trajectoire de la flèche et du portail, v0 est la vitesse initiale, theta est l'angle initial, t est le temps, g est la gravité
 
 def trajectory(v0,theta,t,g, x, y) :
     Xposition = v0 * cos(theta) * t + x
-    Yposition = (-1/2) * ((g*t)**2) + (v0+20) * sin(theta) * t +y
+    Yposition = (-1/2) * ((g*t)**2) + (v0+20) * sin(theta) * t + y
     coordonnee = [Xposition, Yposition]
     return coordonnee
 
@@ -19,3 +22,9 @@ def angle(posiX_joueur,posiY_joueur,posiX_souris,posiY_souris): #calcul de l'ang
     delta_y = posiY_joueur-posiY_souris
     thetarad = atan2(delta_y, delta_x)
     return thetarad
+
+def inclinaison_fleche(v0,theta,t,g):
+    Vx = v0 * cos(theta)
+    Vy = -g*t + v0 * sin(theta)
+    inclinaison_rad=atan2(Vy , Vx)
+    return inclinaison_rad
