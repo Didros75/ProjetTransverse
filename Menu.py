4dc2ef13ba4a053d3d74be868f7f4e96650dd3d2
Arthur <arthur.morineaux@efrei.net>
2025-03-17 10:24:24 +0100

    ajout de la fonction menu

---
+++ Menu.py
@@ -0,0 +1,16 @@
+import pygame
+
+def menu(screen):
+    font=pygame.font.Font(None, 60)
+    text_play = font.render("Jouer", True, (255, 255, 255))
+    play_rect = pygame.Rect(400, 250, 150, 50)
+    pygame.draw.rect(screen, (0, 0, 0), play_rect)
+    screen.blit(text_play, (400, 250))
+    pygame.display.flip()
+    a = True
+    while a:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_rect.collidepoint(event.pos):
+                    a = False
+    return False
